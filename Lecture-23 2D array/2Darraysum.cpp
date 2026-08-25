59dab266e79042fbeb3ef9287ddaaec45330268e
Gurleen Kaur <gurleenkaur75085@gmail.com>
2026-08-25 20:46:46 +0530

    Add row wise sum

--- Lecture-23 2D array/2Darraysum.cpp
+++ Lecture-23 2D array/2Darraysum.cpp
@@ -13,7 +13,7 @@ void printsum(int arr[][3],int row,int col){
 }
 int main(){
     int arr[3][3];
-      //row wise input
+    //row wise input
     for(int row=0;row<3;row++){
         for(int col=0;col<3;col++){
             cin>>arr[row][col];
