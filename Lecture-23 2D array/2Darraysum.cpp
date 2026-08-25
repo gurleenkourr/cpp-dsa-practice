dfb1af9571df7149b7f3fa8e2f645232c21ee5a1
Gurleen Kaur <gurleenkaur75085@gmail.com>
2026-08-25 20:41:21 +0530

    Add row wise sum

---
+++ Lecture-23 2D array/2Darraysum.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+void printsum(int arr[][3],int row,int col){
+    for(row=0;row<3;row++){
+        int sum=0;
+        for(col=0;col<3;col++){
+            sum+=arr[row][col];
+        }
+        cout<<sum<<" ";
+    }
+    cout<<endl;
+}
+int main(){
+    int arr[3][3];
+      //row wise input
+    for(int row=0;row<3;row++){
+        for(int col=0;col<3;col++){
+            cin>>arr[row][col];
+        }
+    }
+    //print
+    for(int row=0;row<3;row++){
+        for(int col=0;col<3;col++){
+  
+            cout<<arr[row][col]<<" ";
+        }
+        cout<<endl;
+    }
+    printsum(arr,3,3);
+}
