484309e841fb18484be1d45ca6f1fcefabc2ad55
Gurleen Kaur <gurleenkaur75085@gmail.com>
2026-08-26 11:49:10 +0530

    Add prime number function

---
+++ Lecture-24 Basic maths/prime.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+bool isprime(int n){
+
+    if(n<=1)
+    return false;
+
+    for(int i=2;i<n;i++){
+        if(n%i==0){
+            return false;
+        }
+        return true;
+    }
+}
+int main(){
+    int n;
+    cin>>n;
+    if(isprime(n)){
+        cout<<"It is a prime number"<<endl;
+    }
+    else{
+        cout<<"It is not a prime number"<<endl;
+    }
+}
