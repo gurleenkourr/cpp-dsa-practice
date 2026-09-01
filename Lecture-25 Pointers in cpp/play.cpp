73212078a6d0ab40593913cdb3b891358a1f5f0e
Gurleen Kaur <gurleenkaur75085@gmail.com>
2026-09-01 23:32:01 +0530

    Implementztion of pointers in cpp

--- Lecture-25 Pointers in cpp/play.cpp
+++ Lecture-25 Pointers in cpp/play.cpp
@@ -2,6 +2,16 @@
 using namespace std;
 
 int main(){
+    /*int *p=0;
+    cout<<*p<<endl;*/
+
+    //two ways to intialize pointer
+    int i=5;
+    int *q=&i;
+    cout<<"Value is: "<<*q<<endl;
+
     int *p=0;
-    cout<<*p<<endl;
+    p=&i;
+    cout<<"Value is: "<<*p<<endl;
+
 }
