776ac3fb0fd5e6aa098da9b34bf56fc518b5e609
Gurleen Kaur <gurleenkaur75085@gmail.com>
2026-09-02 15:56:24 +0530

    Implement poibnters with functions

---
+++ Lecture-26 Pointers with arrays and functions/pointers_functions.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+void print(int*p){
+    cout<<p<<endl;
+    cout<<*p<<endl;
+}
+void update(int *p){
+    //p=p+1;
+    //cout<<p<<endl;
+    *p=*p+1;
+    
+}
+int getsum(int *arr,int n){
+    cout<<endl<<"size : "<<sizeof(arr)<<endl;
+    
+    int sum=0;
+    for (int i=0;i<n;i++){
+        sum+=i[arr];
+    }
+    return sum;
+}
+
+int main(){
+    int value=5;
+    int*p=&value;
+
+    print(p);
+    cout<<"before : "<<*p<<endl;
+    update(p);
+    cout<<"before : "<<*p<<endl;
+
+    int arr[6]={1,2,3,4,5,8};
+    cout<<"sum is : "<<getsum(arr+3,3)<<endl;
+    return 0;
+}
