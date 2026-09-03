4609dac7aa80f42da374751aa9efe6d75d9e42c4
Gurleen Kaur <gurleenkaur75085@gmail.com>
2026-09-03 16:52:53 +0530

    Add dynamic 2d array program

---
+++ Lecture-29 Dynamic Allocation(Part-2)/dynamic2darray.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    //rows and col are same
+    int n;
+    cin>>n;
+    //creation of 2d array
+    int**arr=new int*[n];
+    for(int i=0;i<n;i++){
+        arr[i]=new int[n];
+    }
+
+    //input taking
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cin>>arr[i][j];
+        }
+    }
+
+    //output
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cout<<arr[i][j]<<" ";
+        }cout<<endl;
+    }
+}
