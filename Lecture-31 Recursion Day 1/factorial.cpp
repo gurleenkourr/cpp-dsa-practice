163b6f91d9d2738dab3153c11081cf592d539f09
Gurleen Kaur <gurleenkaur75085@gmail.com>
2026-09-04 20:57:27 +0530

    Implement factorial program using recursion

---
+++ Lecture-31 Recursion Day 1/factorial.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int factorial(int n){
+    //base case
+    if(n==0){
+        return 1; //return is mandatory in base case
+    }
+    int smallerproblem=factorial(n-1);
+    int biggerproblem=n*smallerproblem;
+
+    return biggerproblem;
+}
+int main(){
+    int n;
+    cin>>n;
+    int ans=factorial(n);
+    cout<<ans<<endl;
+    return 0;
+}
