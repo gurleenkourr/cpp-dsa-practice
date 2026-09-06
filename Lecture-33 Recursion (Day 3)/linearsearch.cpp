431a85a8829136d68080c93c2f7b34c08434a19e
Gurleen Kaur <gurleenkaur75085@gmail.com>
2026-09-06 14:00:10 +0530

    Added linear search program using recursion

---
+++ Lecture-33 Recursion (Day 3)/linearsearch.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+//check how calss are going
+void print(int arr[], int size) {
+    cout << "Size of array is " <<size<< endl;
+
+    for(int i=0; i<size; i++) {
+        cout << arr[i] << " ";
+    } cout << endl;
+
+}
+
+bool  linearSearch(int arr[],int size,int key){
+    print(arr,size);
+    //base case
+    if(size==0){
+        return false;
+    }
+    if(arr[0]==key){
+        return true;
+    }
+    else{
+        bool remaining_part=linearSearch(arr+1,size-1,key);
+        return remaining_part;
+    }
+}
+int main(){
+    int arr[5]={3,5,1,2,6};
+    int size=5;
+    int key=2;
+    bool ans=linearSearch(arr,size,key);
+    if(ans){
+        cout<<"Key is found"<<endl;
+    }
+    else{
+        cout<<"Key is not found"<<endl;
+    }
+}
